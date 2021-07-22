ad75f2d27a2f9696d332ee5bde8476bda1c41b46
Mateus Silva <gmateusjose6@gmail.com>
2021-07-22 14:02:48 -0300

    changing base permission to anyone authenticated

--- byebnk/settings.py
+++ byebnk/settings.py
@@ -129,3 +129,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Selecting default permission scheme
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
