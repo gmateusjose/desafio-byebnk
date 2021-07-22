1fb08277232ef0c97d17341008bfdf70f5e68cb3
Mateus Silva <gmateusjose6@gmail.com>
2021-07-22 10:42:56 -0300

    concluding requirement #1

--- api/models.py
+++ api/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Ativo(models.Model):
+	MODALIDADES_DISPONIVEIS = (
+		('RENDA FIXA', 'RENDA FIXA'),
+		('RENDA VARIAVEL', 'RENDA VARIAVEL'),
+		('CRIPTO', 'CRIPTO'),
+	)
 	nome = models.CharField(max_length=250)
-	modalidade = models.CharField(max_length=50)
+	modalidade = models.CharField(max_length=50, choices=MODALIDADES_DISPONIVEIS)
 
 	def __str__(self):
 		return self.nome
