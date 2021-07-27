9fe8fd49fceba36ebceebc6ddc0270ae8c3ee34a
Mateus Silva <gmateusjose6@gmail.com>
2021-07-27 13:14:40 -0300

    formatting serializers accordingly to pep8

--- api/serializers.py
+++ api/serializers.py
@@ -1,23 +1,23 @@
 from rest_framework import serializers
-
 from .models import Ativo, Operacao
 
 
 class AtivoSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Ativo
-		fields = '__all__'
+    class Meta:
+        model = Ativo
+        fields = '__all__'
 
 
 class OperacaoSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Operacao
-		fields = '__all__'
+    class Meta:
+        model = Operacao
+        fields = '__all__'
 
-	usuario = serializers.HiddenField(
-		default=serializers.CurrentUserDefault()
+    usuario = serializers.HiddenField(
+        default=serializers.CurrentUserDefault()
 	)
-	endereco_ip = serializers.SerializerMethodField()
-
-	def get_endereco_ip(self, obj):
-		return f"{self.context['request'].META['REMOTE_ADDR']}"
+    endereco_ip = serializers.SerializerMethodField()
+    
+    def get_endereco_ip(self, obj):
+        return f"{self.context['request'].META['REMOTE_ADDR']}"
+        
